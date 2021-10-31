ebac13226cb33a83563a348bc302a25a12b321d1
Andrew Blomenberg <andrewBlomen@gmail.com>
2021-10-31 10:01:54 -0600

    Changed button mappings and tuned interpolation

--- main.py
+++ main.py
@@ -7,21 +7,30 @@ from numpy import interp
 pygame.display.init()
 pygame.joystick.init()
 
+print('Pan & Tilt: Left stick | Invert tilt: Click left stick')
+print('Select camera 1: X, 2: O, 3: △')
+print('Exit: Options')
+
 SENSITIVITY = [
-    [0, .8, 1],
-    [0, 12, 24]
+    [0, 0.07, 0.3, .8, 1],
+    [0, 0, 2, 12, 24]
 ]
 
 j = pygame.joystick.Joystick(0)
 ips = [f'172.16.0.20{idx}' for idx in range(1, 4)]
+camera_mappings = {1: 0, 2: 1, 3: 2}
 camera_index = 0
+
+invert_tilt = True
 
 NUM_AXES = 12
 axes_state = {idx: 0 for idx in range(NUM_AXES)}
 
 
-def get_pantilt_speed(axis_position: float) -> int:
-    sign = -1 if axis_position >= 0 else 1  # Invert
+def get_pantilt_speed(axis_position: float, invert=True) -> int:
+    sign = 1 if axis_position >= 0 else -1
+    if invert:
+        sign *= -1
 
     return sign * round(
         interp(abs(axis_position), *SENSITIVITY)
@@ -34,22 +43,19 @@ while True:
 
     update_cam = False
     for event in button_presses:
-        if event.dict['button'] == 3:
+        btn_no = event.dict['button']
+        if btn_no == 9:
             exit(0)
 
-        if event.dict['button'] == 5:
+        elif btn_no in camera_mappings:
             update_cam = True
-            camera_index += 1
-        if event.dict['button'] == 3:
-            update_cam = True
-            camera_index -= 1
+            camera_index = camera_mappings[btn_no]
+
+        elif btn_no == 10:
+            invert_tilt = not invert_tilt
+            print('Tilt', 'inverted' if not invert_tilt else 'not inverted')
 
     if update_cam or cam is None:
-        if camera_index >= len(ips):
-            camera_index = 0
-        if camera_index < 0:
-            camera_index = len(ips) - 1
-
         if cam:
             cam.zoom_stop()
             cam.pantilt_stop()
@@ -66,13 +72,9 @@ while True:
     for event in events:
         axes_state[event.dict['axis']] = event.value
 
-    for axis, value in axes_state.items():
-        if abs(value) < .05:
-            axes_state[axis] = 0
-
     cam.pantilt(
         pan_speed=get_pantilt_speed(axes_state[0]),
-        tilt_speed=get_pantilt_speed(axes_state[1])
+        tilt_speed=get_pantilt_speed(axes_state[1], invert_tilt)
     )
     time.sleep(0.07)
 
